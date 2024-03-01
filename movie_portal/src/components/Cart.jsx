a8d500fd98775ed5202710ec11ba8dbd05a4e726
ayushkumarY <ayushkumar777y@gmail.com>
2024-03-01 16:32:20 +0530

    Eight Commit

--- movie_portal/src/components/Cart.jsx
+++ movie_portal/src/components/Cart.jsx
@@ -4,8 +4,8 @@ const Cart = ({ value }) => {
   // console.log(value);
   return (
     <>
-      <div className="bg-gradient-to-bl from-gray-700 via-gray-900 to-black md:w-60 mb-12 md:mx-2 flex flex-col rounded-lg max-w-[30rem] ">
-        <img className="h-[25rem]" src={value.Poster} alt="" />
+      <div className="bg-gradient-to-bl from-gray-700 via-gray-900 to-black md:w-60 mb-12 md:mx-2 flex flex-col rounded-lg max-w-[30rem] overflow-hidden">
+        <img className="h-[25rem] " src={value.Poster} alt="" />
         <div className="m-4">
           <p className="text-center font-mono text-2xl">{value.Title}</p>
           <p className="text-center font-mono text-2xl">Type: {value.Type}</p>
